701a7db61a13c51e595cc1f7f1b9ae562130df1a
konstantin krivonos <konstantin.krivonos@outlook.de>
2024-09-12 08:36:41 +0000

    DDoS Plan etnfernt

--- EHH-Resource.tf
+++ EHH-Resource.tf
@@ -14,24 +14,12 @@ resource "azurerm_network_security_group" "EHH" {
   resource_group_name = azurerm_resource_group.EHH.name
 }
 
-resource "azurerm_network_ddos_protection_plan" "EHH" {
-  name                = "EHH-ddospplan1"
-  location            = azurerm_resource_group.EHH.location
-  resource_group_name = azurerm_resource_group.EHH.name
-
-}
-
 resource "azurerm_virtual_network" "EHH" {
   name                = "KK-vnet-prod"
   location            = azurerm_resource_group.EHH.location
   resource_group_name = azurerm_resource_group.EHH.name
   address_space       = ["192.168.1.0/24"]
   dns_servers         = ["192.168.1.10"]
-
-  ddos_protection_plan {
-    id     = azurerm_network_ddos_protection_plan.EHH.id
-    enable = true
-  }
 
   tags = {
     environment = "Production"
